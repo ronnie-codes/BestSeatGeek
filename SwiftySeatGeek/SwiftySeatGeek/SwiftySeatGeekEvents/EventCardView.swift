7baf88f23bb96eeeabb8d4673aad7efdbe93fa5f
Ronnie Vega <9502820+ronnyvega@users.noreply.github.com>
2021-07-23 16:15:37 -0700

    Fix card view hit box.

--- SwiftySeatGeek/SwiftySeatGeek/SwiftySeatGeekEvents/EventCardView.swift
+++ SwiftySeatGeek/SwiftySeatGeek/SwiftySeatGeekEvents/EventCardView.swift
@@ -9,45 +9,48 @@ struct EventCardView: View {
     @ObservedObject var vm: EventViewModel
     
     var body: some View {
-        VStack {
-            Spacer()
-            HStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: -20) {
-                HStack(alignment: .top) {
-                    AsyncImageView(url: vm.image)
-                        .fixedSize()
-                        .frame(width: 60, height: 60, alignment: .center)
-                        .aspectRatio(contentMode: .fill)
-                        .cornerRadius(7.0)
-                    if vm.favorite {
-                        Image("HeartFilled")
-                            .resizable()
-                            .frame(width: 20, height: 20)
-                            .offset(x: -77, y: -6)
-                    } else {
-                        Image("HeartHollow")
-                            .resizable()
-                            .frame(width: 20, height: 20)
-                            .hidden()
+        HStack {
+            VStack {
+                Spacer()
+                HStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: -20) {
+                    HStack(alignment: .top) {
+                        AsyncImageView(url: vm.image)
+                            .fixedSize()
+                            .frame(width: 60, height: 60, alignment: .center)
+                            .aspectRatio(contentMode: .fill)
+                            .cornerRadius(7.0)
+                        if vm.favorite {
+                            Image("HeartFilled")
+                                .resizable()
+                                .frame(width: 20, height: 20)
+                                .offset(x: -77, y: -6)
+                        } else {
+                            Image("HeartHollow")
+                                .resizable()
+                                .frame(width: 20, height: 20)
+                                .hidden()
+                        }
+                    }
+                    VStack(alignment: .leading) {
+                        Text(vm.title)
+                            .foregroundColor(.black)
+                            .font(.subheadline)
+                            .fontWeight(.semibold)
+                            .fixedSize(horizontal: false, vertical: /*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
+                        Text(vm.location)
+                            .font(.footnote)
+                            .foregroundColor(.secondary)
+                        Text(vm.showtime)
+                            .font(.footnote)
+                            .foregroundColor(.secondary)
                     }
                 }
-                VStack(alignment: .leading) {
-                    Text(vm.title)
-                        .foregroundColor(.black)
-                        .font(.subheadline)
-                        .fontWeight(.semibold)
-                        .fixedSize(horizontal: false, vertical: /*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
-                    Text(vm.location)
-                        .font(.footnote)
-                        .foregroundColor(.secondary)
-                    Text(vm.showtime)
-                        .font(.footnote)
-                        .foregroundColor(.secondary)
-                }
+                Spacer()
             }
+            .padding(.top, 4)
+            .padding(.bottom, 4)
             Spacer()
         }
-        .padding(.top, 4)
-        .padding(.bottom, 4)
     }
 }
 
