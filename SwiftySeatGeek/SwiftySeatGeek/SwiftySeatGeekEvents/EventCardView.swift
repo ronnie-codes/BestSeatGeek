93dc664bdfe2e06a1a0b4aedecf494dfc8595ae7
Ronnie Vega <9502820+ronnyvega@users.noreply.github.com>
2021-07-24 21:42:27 -0700

    Move more code from EventListView into EventCardView

--- SwiftySeatGeek/SwiftySeatGeek/SwiftySeatGeekEvents/EventCardView.swift
+++ SwiftySeatGeek/SwiftySeatGeek/SwiftySeatGeekEvents/EventCardView.swift
@@ -9,43 +9,49 @@ struct EventCardView: View {
     @ObservedObject var viewModel: EventViewModel
 
     var body: some View {
-        HStack {
-            VStack {
-                Spacer(minLength: 12)
-                HStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: -20) {
-                    HStack(alignment: .top) {
-                        AsyncImage(imageUrl: viewModel.image)
-                            .aspectRatio(contentMode: .fill)
-                            .frame(width: 60, height: 60, alignment: .center)
-                            .cornerRadius(7.0)
-                        if viewModel.favorite {
-                            FavoriteImage(favorite: $viewModel.favorite)
-                                .frame(width: 20, height: 20)
-                                .offset(x: -77, y: -6)
-                        } else {
-                            FavoriteImage(favorite: $viewModel.favorite)
-                                .frame(width: 20, height: 20)
-                                .hidden()
+        ZStack(alignment: .leading) {
+            RoundedRectangle(cornerRadius: 6, style: .continuous)
+                .fill(Color.white)
+            HStack {
+                VStack {
+                    Spacer(minLength: 12)
+                    HStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: -16) {
+                        HStack(alignment: .top) {
+                            AsyncImage(imageUrl: viewModel.image)
+                                .aspectRatio(contentMode: .fill)
+                                .frame(width: 60, height: 60, alignment: .center)
+                                .cornerRadius(7.0)
+                            if viewModel.favorite {
+                                FavoriteImage(favorite: $viewModel.favorite)
+                                    .frame(width: 20, height: 20)
+                                    .offset(x: -77, y: -6)
+                            } else {
+                                FavoriteImage(favorite: $viewModel.favorite)
+                                    .frame(width: 20, height: 20)
+                                    .hidden()
+                            }
+                        }
+                        VStack(alignment: .leading) {
+                            Text(viewModel.title)
+                                .foregroundColor(.black)
+                                .font(.subheadline)
+                                .fontWeight(.semibold)
+                                .fixedSize(horizontal: false, vertical: /*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
+                            Text(viewModel.location)
+                                .font(.footnote)
+                                .foregroundColor(.secondary)
+                            Text(viewModel.showtime)
+                                .font(.footnote)
+                                .foregroundColor(.secondary)
                         }
                     }
-                    VStack(alignment: .leading) {
-                        Text(viewModel.title)
-                            .foregroundColor(.black)
-                            .font(.subheadline)
-                            .fontWeight(.semibold)
-                            .fixedSize(horizontal: false, vertical: /*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
-                        Text(viewModel.location)
-                            .font(.footnote)
-                            .foregroundColor(.secondary)
-                        Text(viewModel.showtime)
-                            .font(.footnote)
-                            .foregroundColor(.secondary)
-                    }
+                    Spacer(minLength: 12)
                 }
-                Spacer(minLength: 12)
             }
-            Spacer()
+            .padding(.leading)
+            .padding(.trailing)
         }
+        .shadow(color: Color(UIColor.lightGray), radius: 6, x: 2, y: 0)
     }
 }
 
--- SwiftySeatGeek/SwiftySeatGeek/SwiftySeatGeekEvents/EventListView.swift
+++ SwiftySeatGeek/SwiftySeatGeek/SwiftySeatGeekEvents/EventListView.swift
@@ -15,17 +15,9 @@ struct EventListView: View {
                 LazyVStack(alignment: .leading, spacing: 0) {
                     ForEach(viewModel.eventsGroomed, id: \.id) {
                         let viewModel = EventViewModel(event: $0)
-                        ZStack(alignment: .leading) {
-                            RoundedRectangle(cornerRadius: 6, style: .continuous)
-                                .fill(Color.white)
-
-                            NavigationLink(destination: EventDetailView(viewModel: viewModel)) {
-                                EventCardView(viewModel: viewModel)
-                                    .padding(.leading)
-                                    .padding(.trailing)
-                            }
+                        NavigationLink(destination: EventDetailView(viewModel: viewModel)) {
+                            EventCardView(viewModel: viewModel)
                         }
-                        .shadow(color: Color(UIColor.lightGray), radius: 6, x: 2, y: 0)
                         Spacer(minLength: 15)
                     }
                     if !viewModel.isFull {
